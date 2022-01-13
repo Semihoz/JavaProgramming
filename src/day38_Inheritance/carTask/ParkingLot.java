e13fe6d576402db0e2665ad53fabfc9f82f57c00
Semihoz <Semihozdemir02@gmail.com>
2022-01-13 11:47:37 -0500

    initial commit 4

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,15 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Supra" ,"White", 1994, 79000,24000);
+
+        Tesla tesla = new Tesla("Plaid", "Blue", 2022, 100000,0);
+
+        BMW bmw = new BMW("M3", "Dark Grey", 2022, 90000,0);
+
+    }
+
+}
